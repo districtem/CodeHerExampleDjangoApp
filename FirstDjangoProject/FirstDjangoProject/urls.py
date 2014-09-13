4fd45f29eb2539e7c99bef8c94e0021d15b636ac
districtem <williamson.emily1@gmail.com>
2014-09-13 15:35:23 +0000

    adding templates, views, and URLs

--- FirstDjangoProject/FirstDjangoProject/urls.py
+++ FirstDjangoProject/FirstDjangoProject/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from firstApp.views import provider_function_view, PackageClassView
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^provider/(?P<provider_id>\d+)/$', provider_function_view, name='provider'),
+    url(r'^package/(?P<package_id>\d+)/$', PackageClassView.as_view(), name='package'),
 )
--- FirstDjangoProject/firstApp/views.py
+++ FirstDjangoProject/firstApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+
+from firstApp.models import CablePackage, CableProvider
 
 # Create your views here.
+
+def provider_function_view(request, provider_id):
+    return HttpResponse('Here is your Provider: %s' % provider_id)
+  
+
+class PackageClassView(ListView):
+    template_name = 'package_list_view.html'
+    
+    model = CablePackage
